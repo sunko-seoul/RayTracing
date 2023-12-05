39f2abe5018ab561a17b4d2962928177b8bb0b55
Sunghyun Ko <sunko@cx2r2s10.42seoul.kr>
2023-12-05 14:31:24 +0900

    Add Sampeling function

--- camera.h
+++ camera.h
@@ -9,8 +9,9 @@
 class camera
 {
 	public:
-		double	aspect_ratio = 1.0;		// Ratio of image width over height
-		int		image_width = 100;		// Rendered image width in pixel count
+		double	aspect_ratio = 1.0;		 // Ratio of image width over height
+		int		image_width = 100;		 // Rendered image width in pixel count
+		int		samples_per_pixel = 10;	 // Count of random samples for each pixel
 		void	render(const hittable& world)
 		{
 			initialize();
@@ -20,12 +21,13 @@ class camera
 				std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
 				for (int i = 0; i < image_width; ++i)
 				{
-					auto pixel_center = pixel100_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
-					auto ray_direction = pixel_center - center;
-					ray r(center, ray_direction);
-
-					color	pixel_color = ray_color(r, world);
-					write_color(std::cout, pixel_color);
+					color pixel_color(0, 0, 0);
+					for (int sample = 0; sample < samples_per_pixel; ++sample)
+					{
+						ray r = get_ray(i, j);
+						pixel_color += ray_color(r, world);
+					}
+					write_color(std::cout, pixel_color, samples_per_pixel);
 				}
 			}
 			std::clog << "\rDone.          \n";
@@ -67,6 +69,22 @@ class camera
 			vec3 unit_direction = unit_vector(r.direction());
 			auto a = 0.5 * (unit_direction.y() + 1.0);
 			return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
+		}
+		ray	get_ray(int i, int j) const
+		{
+			auto pixel_center = pixel100_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
+			auto pixel_sample = pixel_center + pixel_sample_square();
+
+			auto ray_origin = center;
+			auto ray_direction = pixel_sample - ray_origin;
+
+			return ray(ray_origin, ray_direction);
+		}
+		vec3	pixel_sample_square() const
+		{
+			auto px = -0.5 + random_double();
+			auto py = -0.5 + random_double();
+			return (px * pixel_delta_u) + (py * pixel_delta_v);
 		}
 };
 
