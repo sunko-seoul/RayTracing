70be7101faebee79a94d0a1f177a37279ea5ffc0
Sunghyun Ko <sunko@cx2r1s1.42seoul.kr>
2023-12-05 23:42:07 +0900

    11

--- sphere.h
+++ sphere.h
@@ -3,13 +3,14 @@
 
 # include "hittable.h"
 # include "vec3.h"
+# include "rtweekend.h"
 
 class sphere : public hittable
 {
 	private:
-		point3				center;
-		double				radius;
-		shared_ptr<matrial>	mat;
+		point3					center;
+		double					radius;
+		shared_ptr<material>	mat;
 	public:
 		sphere(point3 _center, double _radius, shared_ptr<material> _material)
 			: center(_center), radius(_radius), mat(_material) {}
--- vec3.h
+++ vec3.h
@@ -52,6 +52,11 @@ class vec3
 		{
 			return e[0] * e[0] + e[1] * e[1] + e[2] * e[2];
 		}
+		bool	near_zero() const
+		{
+			auto s = 1e-8;
+			return (fabs(e[0]) < s) && (fabs(e[1]) < s) && (fabs(e[2]) < s);
+		}
 		static vec3 random()
 		{
 			return vec3(random_double(), random_double(), random_double());
@@ -77,6 +82,11 @@ inline vec3 operator+(const vec3 &u, const vec3 &v)
 inline vec3 operator-(const vec3 &u, const vec3 &v)
 {
 	return vec3(u.e[0] - v.e[0], u.e[1] - v.e[1], u.e[2] - v.e[2]);
+}
+
+inline vec3 operator*(const vec3 &u, const vec3 &v)
+{
+	return vec3(u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2]);
 }
 
 inline vec3 operator*(double t, const vec3 &v)
@@ -133,6 +143,19 @@ inline vec3 random_on_hemisphere(const vec3& normal)
 		return on_unit_sphere;
 	else
 		return -on_unit_sphere;
+}
+
+vec3	reflect(const vec3& v, const vec3& n)
+{
+	return v - 2*dot(v, n)*n;
+}
+
+inline vec3 refract(const vec3& uv, const vec3& n, double etai_over_etat)
+{
+	auto cos_theta = fmin(dot(-uv, n), 1.0);
+	vec3 r_out_perp = etai_over_etat * (uv + cos_theta * n);
+	vec3 r_out_parallel = -sqrt(fabs(1.0 - r_out_perp.length_squared())) * n;
+	return r_out_perp + r_out_parallel;
 }
 
 #endif
